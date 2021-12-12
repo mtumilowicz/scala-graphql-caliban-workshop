882e82df510d4b2b080057ab65cf0deb37582c10
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-12 19:42:21 +0100

    CustomerGraphQlQueries.scala

--- src/main/scala/app/gateway/customer/CustomerGraphQlQueries.scala
+++ src/main/scala/app/gateway/customer/CustomerGraphQlQueries.scala
@@ -1,13 +1,16 @@
 package app.gateway.customer
 
-import app.domain.{Customer, CustomerId, CustomerServiceEnv}
+import app.domain.{CustomerId, CustomerServiceEnv}
+import app.gateway.customer.out.CustomerApiOutput
 import app.infrastructure.config.customer.CustomerServiceProxy
 import zio.URIO
 
-case class CustomerGraphQlQueries private (findById: CustomerId => URIO[CustomerServiceEnv, Option[Customer]])
+case class CustomerGraphQlQueries private (findById: CustomerId => URIO[CustomerServiceEnv, Option[CustomerApiOutput]])
 
 object CustomerGraphQlQueries {
 
-  def apply(): CustomerGraphQlQueries =
-    CustomerGraphQlQueries(id => CustomerServiceProxy.getById(id))
+  def apply(): CustomerGraphQlQueries = {
+
+    CustomerGraphQlQueries(id => CustomerServiceProxy.getById(id).map(_.map(CustomerApiOutput("", _))))
+  }
 }
