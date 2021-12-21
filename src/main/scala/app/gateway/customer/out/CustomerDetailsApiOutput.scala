7af85c29a8d6a5f68640d2fe2f4fcf6485f1e06e
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-21 23:16:31 +0100

    typo

--- src/main/scala/app/gateway/customer/out/CustomerDetailsApiOutput.scala
+++ src/main/scala/app/gateway/customer/out/CustomerDetailsApiOutput.scala
@@ -3,9 +3,9 @@ package app.gateway.customer.out
 import app.domain.customerdetails.CustomerDetails
 import zio.query.UQuery
 
-case class CustomerDetailsApiOutput(id: String, paid: Boolean)
+case class CustomerDetailsApiOutput(id: String, locked: Boolean)
 
 object CustomerDetailsApiOutput {
   def fromDomain(detailsQuery: UQuery[Option[CustomerDetails]]): UQuery[Option[CustomerDetailsApiOutput]] =
-    detailsQuery.map(_.map(details => CustomerDetailsApiOutput(id = details.id.value, paid = details.locked)))
+    detailsQuery.map(_.map(details => CustomerDetailsApiOutput(id = details.id.value, locked = details.locked)))
 }
