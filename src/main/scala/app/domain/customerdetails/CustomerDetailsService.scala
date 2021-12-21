1c05ff2ac07115dc23cdaefcfdeb829659dc0189
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-21 20:49:31 +0100

    CustomerDetails moving datasource to service

--- src/main/scala/app/domain/customerdetails/CustomerDetailsService.scala
+++ src/main/scala/app/domain/customerdetails/CustomerDetailsService.scala
@@ -1,11 +1,28 @@
 package app.domain.customerdetails
 
 import app.domain.customer.CustomerId
-import zio.UIO
+import zio.{Chunk, UIO}
+import zio.query.{DataSource, Request, UQuery, ZQuery}
 
 case class CustomerDetailsService(repository: CustomerDetailsRepository) {
+
+  private val dataSource: DataSource[Any, GetCustomerDetails] =
+    DataSource.fromFunctionBatchedM("CustomerDetailsDataSource")(
+      requests => {
+        println(requests)
+        val ids = requests.map(_.id)
+        getByIds(ids.toSet).map(detailsMap => restoreOrder(ids, detailsMap))
+      }
+    )
+
+  def getById(id: CustomerId): UQuery[Option[CustomerDetails]] =
+    ZQuery.fromRequest(GetCustomerDetails(id))(dataSource)
 
   def getByIds(customerIds: Set[CustomerId]): UIO[Map[CustomerId, CustomerDetails]] =
     repository.getById(customerIds)
 
+  private def restoreOrder(ids: Chunk[CustomerId], map: Map[CustomerId, CustomerDetails]): Chunk[Option[CustomerDetails]] =
+    ids.map(id => map.get(id))
+
+  private case class GetCustomerDetails(id: CustomerId) extends Request[Nothing, Option[CustomerDetails]]
 }
