ef92f38a2610e2d2a9da5acd2d1cec266f516b8e
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-21 17:41:02 +0100

    CustomerDetails pt2

--- src/main/scala/app/domain/customer/NewCustomerCommand.scala
+++ src/main/scala/app/domain/customer/NewCustomerCommand.scala
@@ -1,9 +1,6 @@
 package app.domain.customer
 
-import app.domain.order.CustomerDetails
-
-
 case class NewCustomerCommand(name: String, locked: Boolean) {
   def toCustomer(id: CustomerId): Customer =
-    Customer(id = id, name = name, details = CustomerDetails(id, true) ,locked = locked)
+    Customer(id = id, name = name,locked = locked)
 }
