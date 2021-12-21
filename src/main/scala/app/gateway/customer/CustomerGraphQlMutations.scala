91eff8d5ae45c360a7d9025f04c28328bfdd0a20
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-21 20:56:51 +0100

    CreatedCustomerApiOutput

--- src/main/scala/app/gateway/customer/CustomerGraphQlMutations.scala
+++ src/main/scala/app/gateway/customer/CustomerGraphQlMutations.scala
@@ -2,11 +2,11 @@ package app.gateway.customer
 
 import app.domain.customer._
 import app.gateway.customer.in.NewCustomerApiInput
-import app.gateway.customer.out.CustomerApiOutput
+import app.gateway.customer.out.CreatedCustomerApiOutput
 import app.infrastructure.config.customer.CustomerServiceProxy
 import zio.RIO
 
-case class CustomerGraphQlMutations private(create: NewCustomerApiInput => RIO[CustomerServiceEnv, CustomerApiOutput],
+case class CustomerGraphQlMutations private(create: NewCustomerApiInput => RIO[CustomerServiceEnv, CreatedCustomerApiOutput],
                                             deleteById: CustomerId => RIO[CustomerServiceEnv, CustomerId])
 
 object CustomerGraphQlMutations {
@@ -15,7 +15,7 @@ object CustomerGraphQlMutations {
     val rootUri = s"$baseUrl/customers"
 
     CustomerGraphQlMutations(
-      input => CustomerServiceProxy.create(input.toDomain).map(CustomerApiOutput.fromDomain(rootUri, _)),
+      input => CustomerServiceProxy.create(input.toDomain).map(CreatedCustomerApiOutput.fromDomain(rootUri, _)),
       id => CustomerServiceProxy.delete(id)
     )
   }
