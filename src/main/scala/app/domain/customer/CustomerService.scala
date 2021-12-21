37510e842b4458f09eea687a08a0b1e0452383e0
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-21 19:28:25 +0100

    CustomerDetails ZQuery

--- src/main/scala/app/domain/customer/CustomerService.scala
+++ src/main/scala/app/domain/customer/CustomerService.scala
@@ -1,15 +1,25 @@
 package app.domain.customer
 
+import app.domain.order.CustomerDetails
+import zio.query.{DataSource, Request, UQuery, ZQuery}
 import zio.stream.UStream
-import zio.{Task, UIO}
+import zio.{Task, UIO, ZIO}
 
 case class CustomerService(idService: IdService, repository: CustomerRepository) {
+
+  case class GetCustomerDetails(id: CustomerId) extends Request[Nothing, CustomerDetails]
+  val CustomerDetailsDataSource: DataSource[Any, GetCustomerDetails] =
+    DataSource.fromFunctionBatchedM("CustomerDetailsDataSource")(
+      requests => ZIO.succeed(requests.map(x => x.id).map(id => CustomerDetails(id, true)))
+    )
+  def getCustomerDetails(id: CustomerId): UQuery[CustomerDetails] = ZQuery.fromRequest(GetCustomerDetails(id))(CustomerDetailsDataSource)
 
   def createdCustomers: UStream[Customer] =
     repository.createdCustomers
 
-  def getById(id: CustomerId): UIO[Option[CustomerView]] =
-    repository.getById(id).map(_.map(_.toView()))
+  def getById(id: CustomerId): UIO[Option[CustomerView]] = {
+    repository.getById(id).map(_.map(_.toView(getCustomerDetails(id))))
+  }
 
   def delete(id: CustomerId): Task[CustomerId] =
     repository.delete(id)
